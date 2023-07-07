4f34d5e7e9b78616a09a2d86a4fa0a12ccb1ed1e
Amiel Brencis Salipande <absalipande@gmail.com>
2023-07-07 14:42:16 +0800

    using parent data

--- svelte-loading-data/src/routes/products/+layout.js
+++ svelte-loading-data/src/routes/products/+layout.js
@@ -1,10 +1,8 @@
-export const load = async (loadEvent) => {
-	const { fetch } = loadEvent;
+export const load = async ({ fetch, parent }) => {
+	const parentData = await parent();
+	const { username } = parentData;
 	const title = 'Featured products';
 	const response = await fetch('http://localhost:4000/featured-products');
 	const featuredProducts = await response.json();
-	return {
-		title,
-		featuredProducts
-	};
+	return { username, title, featuredProducts };
 };
