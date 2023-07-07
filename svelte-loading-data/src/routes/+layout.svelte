4887ed13b7cc3d300f8cdeadf6dc641a469158f3
Amiel Brencis Salipande <absalipande@gmail.com>
2023-07-07 14:52:18 +0800

    using children data

--- svelte-loading-data/src/routes/+layout.svelte
+++ svelte-loading-data/src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
-    export let data
-    const { username } = data
+	import { page } from '$app/stores';
+	export let data;
+	const { username } = data;
 </script>
+
+{#if $page.data.notifcation}
+	<p>{$page.data.notifcation}</p>
+{/if}
 
 <div>Welcome, {username}</div>
 <slot />
